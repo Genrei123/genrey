d30c15f08b9d06c07ab9cbd9440a2e2952fe05d0
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-04-15 19:52:47 +0800

    added carousel

--- portfolio-blog-site/nextjs-app/app/components/Certificates.tsx
+++ portfolio-blog-site/nextjs-app/app/components/Certificates.tsx
@@ -34,7 +34,7 @@ const Certificates = () => {
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
+                    <a href="#slide5" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide5" className="carousel-item relative w-full">
@@ -42,7 +42,7 @@ const Certificates = () => {
                     src="./java.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>

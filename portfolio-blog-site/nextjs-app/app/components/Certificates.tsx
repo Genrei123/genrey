52b26107113f8251dc01e61847d5b96b36879fc8
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-04-18 17:55:57 +0800

    Added gitgoing

--- portfolio-blog-site/nextjs-app/app/components/Certificates.tsx
+++ portfolio-blog-site/nextjs-app/app/components/Certificates.tsx
@@ -6,7 +6,7 @@ const Certificates = () => {
                     src="./bpi.jpg"
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                    <a href="#slide5" className="btn btn-circle">❮</a>
+                    <a href="#slide6" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
@@ -43,6 +43,16 @@ const Certificates = () => {
                     className="w-full" />
                 <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
                     <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide6" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+
+            <div id="slide6" className="carousel-item relative w-full">
+                <img
+                    src="./gitgoing.jpg"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide5" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>
